3bbd91e4d29ac32b19108e641730762e939bdd11
Muhammad-Sharif Moustafa <mshmoustafa@gmail.com>
2017-05-14 01:32:00 -0400

    added comments

--- src/oeis.js
+++ src/oeis.js
@@ -4,19 +4,28 @@ OEIS.proxy = "http://crossorigin.me/";
 OEIS.endpoint = OEIS.proxy + "http://oeis.org/";
 OEIS.lastRequest = "";
 
+// Searches the OEIS by the sequence's ID (e.g. A000045)
+// id: The ID of the sequence. Must be a string preceded by an 'A' and have six digits. Example: "A000045"
+// format: "json" or "text"
+// callback: a function that is called with the search results passed as an argument. The argument is either a JavaScript object if the format specified was "json" or text otherwise.
 OEIS.searchByID = function (id, format, callback) {
-    URL = OEIS.endpoint + "search?" + "q=" + "id:A" + id + "&" + "fmt=" + format;
-    OEIS.doRequest(URL, callback);
+    URL = OEIS.endpoint + "search?" + "q=" + "id:" + id + "&" + "fmt=" + format;
+    OEIS.doRequest(URL, format, callback);
 };
 
+// Searches the OEIS by terms in a sequence (e.g. 1,1,2,3,5)
+// terms: an array of the terms
+// format: "json" or "text"
+// callback: a function that is called with the search results passed as an argument. The argument is either a JavaScript object if the format specified was "json" or text otherwise.
 OEIS.searchByTerms = function (terms, format, callback) {
     URL = OEIS.endpoint + "search?" + "q=" + terms.toString() + "&" + "fmt=" + format;
-    OEIS.doRequest(URL, callback);
+    OEIS.doRequest(URL, format, callback);
 }
 
 // Based heavily on code found on www.html5rocks.com by
 // Monsur Hossain.
 // https://www.html5rocks.com/en/tutorials/cors/
+// Convenience method that creates and returns an XMLHttpRequest. Not meant for public use. For making requests, please use the OEIS.searchBy() family of functions.
 OEIS.createCORSRequest = function (method, url) {
     var xhr = new XMLHttpRequest();
     if ("withCredentials" in xhr) {
@@ -41,6 +50,7 @@ OEIS.createCORSRequest = function (method, url) {
     return xhr;
 };
 
+// Convenience method that executes an XMLHttpRequest. Not meant for public use. For making requests, please use OEIS.searchBy() family of functions.
 OEIS.doRequest = function (URL, callback) {
     var xhr = OEIS.createCORSRequest("GET", URL);
     if (!xhr) {
